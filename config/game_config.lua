7772a6f66516377843e1449091b6f96890469f82
Evie Dischinger <eviedischinger@gmail.com>
2026-04-30 16:26:35 -0400

    trying to fix a bug

--- config/game_config.lua
+++ config/game_config.lua
@@ -8,8 +8,8 @@ return {
 	map = {
 		max_x = 500,
 		max_y = 500,
-		max_z = 7,
-		min_z = -7,
+		max_z = 6,
+		min_z = -4,
 	},
 	timing = {
 		turn_delay = 0.125,
--- map/tile_types.lua
+++ map/tile_types.lua
@@ -3,7 +3,7 @@ return {
 		name = "grass",
 		chars = { "." },
 		walkable = true,
-		color = { 0.22, 0.45, 0.25, 1 },
+		color = { 0.2, 0.45, 0.25, 1 },
 		transparent = true,
 		covers = true,
 	},
