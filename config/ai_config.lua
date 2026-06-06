b418511f60a5979eaae91bf566d4a404184bd362
Evie Dischinger <eviedischinger@gmail.com>
2026-06-05 22:46:38 -0400

    Refactoring AI to be smarter when chasing enemies

--- config/ai_config.lua
+++ config/ai_config.lua
@@ -1,6 +1,10 @@
 return {
 	wander_chance = 5, -- 1-in-N roll each idle tick
 	wander_range = 10, -- +/- tiles from current position
-	turns_to_idle = 20, -- countdown before giving up chase/wander
+	wander_turns = 20, -- countdown before giving up wander
+	search_turns = 30,
+	search_radius = 10,
+	search_lead = 5,
+	search_attempts = 8, -- tries to roll a walkable search probe before falling back to the vanish point
 	activation_range = 50,
 }
