dc8fc6d8f7c81098f64382a69b131e810764960c
Evie Dischinger <eviedischinger@gmail.com>
2026-06-18 08:50:24 -0400

    cursor scrolling

--- engine/cursor.lua
+++ engine/cursor.lua
@@ -1,6 +1,6 @@
 local cursor = {}
 local moused_entity_list = nil
-local moused_entity_i = 1 --TODO, should not be a magic number, should be scrollable
+local moused_entity_i = 1
 local moused_x = nil
 local moused_y = nil
 local sel_x, sel_y = nil, nil
@@ -15,13 +15,7 @@ function cursor.get_moused_coords()
 end
 
 local flagged = nil
-function cursor.set_moused_entity(entity_list)
-	if moused_x ~= sel_x or moused_y ~= sel_y then
-		sel_x, sel_y = moused_x, moused_y
-		moused_entity_i = 1
-	end
-	moused_entity_list = (entity_list and #entity_list > 0) and entity_list or nil
-	moused_entity_i = math.min(moused_entity_i, (moused_entity_list and #moused_entity_list) or 1)
+local function apply_moused_selection()
 	local new = cursor.get_moused_entity()
 	if new ~= flagged then
 		if flagged then
@@ -34,6 +28,25 @@ function cursor.set_moused_entity(entity_list)
 		return true
 	end
 	return false
+end
+
+function cursor.set_moused_entity(entity_list)
+	if moused_x ~= sel_x or moused_y ~= sel_y then
+		sel_x, sel_y = moused_x, moused_y
+		moused_entity_i = 1
+	end
+	moused_entity_list = (entity_list and #entity_list > 0) and entity_list or nil
+	moused_entity_i = math.min(moused_entity_i, (moused_entity_list and #moused_entity_list) or 1)
+	return apply_moused_selection()
+end
+
+function cursor.scroll_entity(delta)
+	local n = moused_entity_list and #moused_entity_list or 0
+	if n <= 1 then
+		return false
+	end
+	moused_entity_i = (moused_entity_i - 1 + delta) % n + 1
+	return apply_moused_selection()
 end
 
 function cursor.get_moused_entity()
