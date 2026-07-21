bda5188d582de07451162904423f9d909ddfbaac
Evie Dischinger <eviedischinger@gmail.com>
2026-07-20 22:29:43 -0400

    shrubs need to be taller than actors

--- map/tile_types.lua
+++ map/tile_types.lua
@@ -34,7 +34,7 @@ return {
 		transparent = true,
 		covers = true,
 		applies_status = { "obscured", silent = true },
-		natural_height = 0.25,
+		natural_height = 0.3,
 	},
 	road = {
 		name = "Road",
