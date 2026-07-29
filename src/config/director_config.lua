5b5595da1dd1407c265feb84be7913d56236bf97
Evie Dischinger <eviedischinger@gmail.com>
2026-07-28 23:11:55 -0400

    Changes to spawn rates

--- src/config/director_config.lua
+++ src/config/director_config.lua
@@ -5,22 +5,22 @@ return {
 	max_path_checks = 5,
 
 	spawn_list = {
-		{ name = "zombie", price = 20, weight = 10 },
-		{ name = "shambler", price = 20, weight = 10 },
+		{ name = "zombie", price = 20, weight = 20 },
+		{ name = "shambler", price = 20, weight = 20 },
 		{ name = "skeleton", price = 30, weight = 6 },
 		{ name = "vampire", price = 40, weight = 6 },
-		{ name = "ogre", price = 60, weight = 2 },
+		{ name = "ogre", price = 60, weight = 1 },
 	},
 
 	modifiers = {
 		{ name = "normal", price = 0, weight = 30 },
-		{ name = "hunter", price = 10, weight = 10 },
+		{ name = "hunter", price = 10, weight = 5 },
 	},
 
 	pack_sizes = {
 		{ size = 1, weight = 10 },
 		{ size = 2, weight = 5 },
-		{ size = 3, weight = 2 },
+		{ size = 3, weight = 1 },
 	},
 
 	-- distance band from the player for the pack leader
@@ -31,7 +31,7 @@ return {
 	max_pack_spawn_range = 10,
 
 	-- dread accrues each tick and is spent on packs
-	dread_inc = 2,
+	dread_inc = 1,
 	min_dread_spawn = 40,
 	max_dread_spawn = 200,
 	-- percent chance of a spawn attempt, lerped across the dread band
