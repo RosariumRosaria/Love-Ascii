73e42a022724db342b28be78da5f6b1465882616
Evie Dischinger <eviedischinger@gmail.com>
2026-07-31 19:51:32 -0400

    for some event plumbing

--- src/sim/status_types.lua
+++ src/sim/status_types.lua
@@ -91,6 +91,7 @@ return {
 		description = "Blocked interaction and increased health.",
 		color = { 1, 0.8, 0.5 },
 		hp = 10,
+		absorb_noun = "barricade",
 		tags = { absorbs = true, disables_interaction = true, remove_when_empty = true, applies_to_props = true },
 		visual = { tint = { 1, 0.8, 0.5 } },
 	},
